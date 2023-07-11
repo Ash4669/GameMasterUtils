ced75fa398832968d7e69543ce7f533cf70939ce
Ashley Whitehouse <ash4669@hotmail.co.uk>
2023-07-11 23:56:19 +0100

    Got basic android setup working with emulator.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "semi": true,
+  "tabWidth": 2,
+  "printWidth": 100,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
